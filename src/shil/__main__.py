bdd58f446f65dbb232d66fbe8314c96728e61557
mvr <matt@example.com>
2023-08-16 13:42:21 -0700

    docs; smoke tests; cli interface

--- src/shil/__main__.py
+++ src/shil/__main__.py
@@ -8,33 +8,52 @@ from . import fmt
 
 LOGGER = lme.get_logger(__name__)
 
+@cli.click.group
+def entry():
+    """CLI tool for `shil` library"""
 
-@cli.click.argument("filename")
-@cli.click.command
-def entry(filename: str = "/dev/stdin"):
-    """CLI tool for shil
-    :param filename: str:  (Default value = '/dev/stdin')
+DEFAULT_INPUT_FILE = "/dev/stdin"
+
+# @cli.click.argument("filename", default=DEFAULT_INPUT_FILE)
+@cli.click.flag("--rich", help='use rich output')
+@entry.command
+def invoke(
+    rich:bool=False,
+    ) -> None:
+    """Invocation tool for (line-oriented) bash"""
+
+
+@entry.command(name='fmt')
+@cli.click.argument("filename", default=DEFAULT_INPUT_FILE)
+@cli.click.flag("--rich", help='use rich output')
+def _fmt(
+    filename: str = DEFAULT_INPUT_FILE, 
+    rich:bool=False,
+    ) -> None:
+    """
+    Pretty-printer for (line-oriented) bash
     """
     if filename == "-":
-        filename = "/dev/stdin"
+        filename = DEFAULT_INPUT_FILE
     try:
         with open(filename) as fhandle:
             text = fhandle.read()
     except FileNotFoundError:
-        LOGGER.warning("input is not a file; parsing as string")
+        LOGGER.warning(f"input @ {filename} is not a file; parsing as string")
         text = filename
-    formatted = fmt(text, filename=filename)
-    print(text)
-    print()
-    print(formatted)
+    return report(fmt(text),rich=rich)
 
-    lme.CONSOLE.print(
-        app.Syntax(
-            fmt(text, filename=filename),
-            "bash",
-            word_wrap=True,
+def report(output, rich=False) -> None:
+    if rich:
+        lme.CONSOLE.print(
+            app.Syntax(
+                output,
+                "bash",
+                word_wrap=True,
+            )
         )
-    )
+    else:
+        print(output)
 
 
 if __name__ == "__main__":
--- tests/smoke/test.sh
+++ tests/smoke/test.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
+
+# test command-line is available 
 python -m shil --help
+python -m shil fmt --help
+python -m shil invoke --help
+
+# test basic formatting
+printf '\
+  docker run -it --rm -v `pwd`:/workspace \
+  -w /workspace --entrypoint bash \
+  some-image -c "some command"' \
+| python -m shil fmt - 
+
+# test rich output
+printf '\
+  docker run -it --rm -v `pwd`:/workspace \
+  -w /workspace --entrypoint bash \
+  some-image -c "some command"' \
+| python -m shil fmt - --rich 
